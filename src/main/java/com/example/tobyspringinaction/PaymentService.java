2d8eaa199753857799760e82d8c94b0e38e082dd
autumn-sun91 <dpwe231@gmail.com>
2025-04-28 22:54:49 +0900

    결제 도메인 객체 프로퍼티 선언

--- src/main/java/com/example/tobyspringinaction/PaymentService.java
+++ src/main/java/com/example/tobyspringinaction/PaymentService.java
@@ -1,7 +1,19 @@
 package com.example.tobyspringinaction;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        // 환율 가져오기
+        // 금액 계산
+        // 유효 시간 계산
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(1L, "USd", BigDecimal.valueOf(50.7));
+        System.out.println("payment = " + payment);
     }
 }
