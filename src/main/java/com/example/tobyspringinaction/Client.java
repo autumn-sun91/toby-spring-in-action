c5e2e1d267dec09c5537f235aaf267080231e74a
autumn-sun91 <dpwe231@gmail.com>
2025-04-29 22:28:37 +0900

    관계 책임 설정 분리, 구현체를 사용하는쪽에서 관리하는것이 아닌 외부에서 생성해서 파라미터로 전달

--- src/main/java/com/example/tobyspringinaction/Client.java
+++ src/main/java/com/example/tobyspringinaction/Client.java
@@ -6,7 +6,8 @@ import java.math.BigDecimal;
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
+//        PaymentService paymentService = new PaymentService(new WebApiExRatePaymentProvider());
+        PaymentService paymentService = new PaymentService(new FixExRatePaymentProvider());
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(50.7));
         System.out.println("payment = " + payment);
     }
