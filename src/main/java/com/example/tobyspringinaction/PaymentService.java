72384088b6b5e44f1c274e6ad044e69ae4f07392
autumn-sun91 <dpwe231@gmail.com>
2025-04-29 21:43:30 +0900

    고정 환율로 처리하는 구현으로 교체

--- src/main/java/com/example/tobyspringinaction/PaymentService.java
+++ src/main/java/com/example/tobyspringinaction/PaymentService.java
@@ -5,15 +5,15 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
-    private WebApiExRatePaymentProvider exRateProvider;
+    private FixExRatePaymentProvider exRateProvider;
 
     public PaymentService() {
-        this.exRateProvider = new WebApiExRatePaymentProvider();
+        this.exRateProvider = new FixExRatePaymentProvider();
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
         // 환율 가져오기
-        BigDecimal rate = exRateProvider.getWebBigDecimal(currency);
+        BigDecimal rate = exRateProvider.getBigDecimal(currency);
 
         // 금액 계산
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(rate);
