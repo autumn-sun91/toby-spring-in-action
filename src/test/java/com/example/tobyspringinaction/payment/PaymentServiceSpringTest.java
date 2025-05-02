689b70b23b87b3acb148b20fb2d6ecbe8c238c91
autumn-sun91 <dpwe231@gmail.com>
2025-05-02 23:33:38 +0900

    PaymentService 테스트 추가(3-2) - @ExtendWith, @ContextConfiguration, @Autowired 를 이용한 테스트 진행

--- src/test/java/com/example/tobyspringinaction/payment/PaymentServiceSpringTest.java
+++ src/test/java/com/example/tobyspringinaction/payment/PaymentServiceSpringTest.java
@@ -3,8 +3,12 @@ package com.example.tobyspringinaction.payment;
 import com.example.tobyspringinaction.TestObjectFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -12,15 +16,23 @@ import java.math.BigDecimal;
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
+// @ContextConfiguration 이용 시 스프링 기능을 확장해달라고 JUnit에게 알려줘야됨
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
 
-    @Test
-    @DisplayName("prepare 메소드가 요구사항 3가지(환율 정보 가져오는지, 원환 환산, 원화 환산 유효시간)를 잘 충족했는지 검증")
-    void prepare() throws IOException {
-        // given
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
-        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+    @Autowired
+    // BeanFactory beanFactory; // 컨테이너가 가지고 있는 BeanFactory 도 Bean 으로 등록되어서 가져올 수 있음
+    PaymentService paymentService;
 
+    @Autowired
+    ExRateProviderStub exRateProviderStub;
+
+    @Test
+    @DisplayName("exRate: 1_000 -> prepare 메소드가 요구사항 3가지(환율 정보 가져오는지, 원환 환산, 원화 환산 유효시간)를 잘 충족했는지 검증")
+    void prepare1_000() throws IOException {
+        // given
+        exRateProviderStub.setExRate(BigDecimal.valueOf(1_000));
         // when
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
@@ -30,9 +42,21 @@ class PaymentServiceSpringTest {
 
         // 원화 환산
         assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
+    }
 
-        // 원화 환산 유효시간
-//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    @Test
+    @DisplayName("exRate: 500 -> prepare 메소드가 요구사항 3가지(환율 정보 가져오는지, 원환 환산, 원화 환산 유효시간)를 잘 충족했는지 검증")
+    void prepare500() throws IOException {
+        // given
+        exRateProviderStub.setExRate(BigDecimal.valueOf(500));
+        // when
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // then
+        // 환율 정보 가져온다
+        assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(500));
+
+        // 원화 환산
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
     }
 }
