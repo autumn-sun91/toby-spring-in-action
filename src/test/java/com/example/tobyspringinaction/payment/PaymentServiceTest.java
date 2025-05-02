a2f27db2f21ea31a991e133a164e96d57b2ee66e
autumn-sun91 <dpwe231@gmail.com>
2025-05-02 23:13:52 +0900

    PaymentService 테스트 추가(2) - stub 객체(테스트 대역)을 이용한 수동 DI로 테스트 진행

--- src/test/java/com/example/tobyspringinaction/payment/PaymentServiceTest.java
+++ src/test/java/com/example/tobyspringinaction/payment/PaymentServiceTest.java
@@ -1,38 +1,65 @@
 package com.example.tobyspringinaction.payment;
 
-import com.example.tobyspringinaction.exrate.WebApiExRatePaymentProvider;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentServiceTest {
 
+    static class ExRateProviderStub implements ExRateProvider {
+
+        private BigDecimal exRate;
+
+        public BigDecimal getExRate() {
+            return exRate;
+        }
+
+        public void setExRate(BigDecimal exRate) {
+            this.exRate = exRate;
+        }
+
+        public ExRateProviderStub(BigDecimal exRate) {
+            this.exRate = exRate;
+        }
+
+        @Override
+        public BigDecimal getExRate(String currency) throws IOException {
+            return exRate;
+        }
+    }
+
     @Test
-    @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
+    @DisplayName("prepare 메소드가 요구사항 3가지(환율 정보 가져오는지, 원환 환산, 원화 환산 유효시간)를 잘 충족했는지 검증")
     void prepare() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
+
+        // 원화 환산 유효시간
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    @NonNull
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
         // given
-        PaymentService paymentService = new PaymentService(new WebApiExRatePaymentProvider());
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
 
         // when
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
         // then
         // 환율 정보 가져온다
-        assertThat(payment.getExRate()).isNotNull();
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
 
         // 원화 환산
-        assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-
-        // 원화 환산 유효시간
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
