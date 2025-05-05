d7edf03735c8e751dcfdc3cd64b0f1477991c76c
autumn-sun91 <dpwe231@gmail.com>
2025-05-06 00:42:13 +0900

    Transaction 테스트 -> 정말 하나의 작업 단위로 묶여있는가?
    - 테스트 코드 중간에 일부러 예외를 던짐

--- src/test/java/com/example/tobyspringinaction/order/OrderServiceSpringTest.java
+++ src/test/java/com/example/tobyspringinaction/order/OrderServiceSpringTest.java
@@ -1,14 +1,20 @@
 package com.example.tobyspringinaction.order;
 
 import com.example.tobyspringinaction.OrderConfig;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = OrderConfig.class)
@@ -16,10 +22,39 @@ public class OrderServiceSpringTest {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
     void createOrder() {
         var order = orderService.createOrder("0100", BigDecimal.TEN);
 
-        Assertions.assertThat(order.getId()).isGreaterThan(0);
+        assertThat(order.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void createdOrders() {
+        List<OrderReq> orderReqs = List.of(new OrderReq("0200", BigDecimal.ONE), new OrderReq("0201", BigDecimal.ONE));
+
+        var orders = orderService.createOrders(orderReqs);
+
+        assertThat(orders).hasSize(2);
+        orders.forEach(order -> assertThat(order.getId()).isGreaterThan(0));
+    }
+
+    @Test
+    void createdDuplicatedOrders() {
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0300", BigDecimal.ONE),
+                new OrderReq("0300", BigDecimal.ONE)
+        );
+
+        assertThatThrownBy(() -> orderService.createOrders(orderReqs))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+        JdbcClient jdbcClient = JdbcClient.create(dataSource);
+
+        var count = jdbcClient.sql("select count(*) from orders where no ='0300';").query(Long.class).single();
+        assertThat(count).isEqualTo(0);
     }
 }
